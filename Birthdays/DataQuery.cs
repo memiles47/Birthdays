b58707e2c55ab927b376c5a63e4f293148830e67
Michael E Miles <michael@miles-me.net>
2017-08-01 03:07:48 -0500

    New class created, all works perfectly. Next up, setting security for classes

--- Birthdays/DataQuery.cs
+++ Birthdays/DataQuery.cs
@@ -1,7 +1,34 @@
+using System;
+using System.Collections.Generic;
+
 namespace Birthdays
 {
     public class DataQuery
     {
-        //Data Query code here
+        public void NameCheck(Dictionary<string, string> birthdays)
+        {
+            while (true)
+            {
+                Console.Write("Enter a name (blank to quit): ");
+                var name = Console.ReadLine();
+                if (name == "")
+                    break;
+
+                if (name != null && birthdays.ContainsKey(name))
+                    Console.WriteLine($"\n{birthdays[name]} is the birthday of {name}");
+
+                else
+                {
+                    Console.WriteLine($"\nI do not have birthday information for {name}");
+                    Console.Write("What is their birthday? ");
+                    var bday = Console.ReadLine();
+                    if (bday != null)
+                    {
+                        if (name != null) birthdays[name] = bday;
+                        Console.WriteLine("\nBirthday database updated\n");
+                    }
+                }
+            }
+        }
     }
 }
--- Birthdays/Program.cs
+++ Birthdays/Program.cs
@@ -7,30 +7,10 @@ namespace Birthdays
         private static void Main()
         {
             var birthday = new BirthdayData();
+            var bdayQuery = new DataQuery();
             var birthdays = birthday.BDayData;
 
-            while (true)
-            {
-                Console.WriteLine("Enter a name (blank to quit)");
-                var name = Console.ReadLine();
-                if (name == "")
-                    break;
-
-                if (name != null && birthdays.ContainsKey(name))
-                    Console.WriteLine($"\n{birthdays[name]} is the birthday of {name}");
-
-                else
-                {
-                    Console.WriteLine($"\nI do not have birthday information for {name}");
-                    Console.Write("What is their birthday? ");
-                    var bday = Console.ReadLine();
-                    if (bday != null)
-                    {
-                        if (name != null) birthdays[name] = bday;
-                        Console.WriteLine("\nBirthday database updated\n");
-                    }
-                }
-            }
+            bdayQuery.NameCheck(birthdays);
 
             Console.WriteLine("\n\nYou quit, press any key to continue");
             Console.ReadLine();
